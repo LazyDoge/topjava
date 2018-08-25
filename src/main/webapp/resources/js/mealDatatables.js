5ac114518908fc883d841a6d215477ca11fcbfb2
LazyDoge <lazywoolf@gmail.com>
2018-08-25 12:26:20 +0300

    HW08 1.1+1.2 meal_ajax_1.0

---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,36 @@
+var ajaxUrl = "ajax/profile/meals/";
+
+
+$(function () {
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ]
+
+    });
+    makeEditable();
+});
